e6e109e9a2e863e0ab7d6cce2025eda8249de329
Amir R Muntasser <arkmuntasser@gmail.com>
2016-02-27 20:53:36 -0700

    js fixes

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $(window).scroll(function() {
+    var h = $("img").height();
+    var top = $(this).scrollTop();
+
+    $("img").css({
+      top: (top / 4) + "px",
+      opacity: (h - top) / h
+    });
+  });
+
+  $(window).resize(function() {
+    var h = $("img").height();
+    var top = $(window).scrollTop();
+
+    $(".hero").css({ height: h + "px" });
+
+    if(top > 0) {
+      $("img").css({
+        top: (top / 4) + "px",
+        opacity: (h - top) / h
+      });
+    }
+  });
+
+  var h = $("img").height();
+
+  $(".hero").attr("style", "height : " + h + "px;");
+  $(window).resize();
+});
